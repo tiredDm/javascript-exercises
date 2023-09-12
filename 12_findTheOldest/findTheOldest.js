dd0942249b03499a490e3c7ded20c2c6641d02a2
tiredDm <ronpri1@gmail.com>
2023-09-12 10:21:03 -0400

    12 babyyy

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    
+    for (let i = 0; i < people.length; i++) {
+        if(!(people[i].hasOwnProperty('yearOfDeath'))){
+            people[i]['yearOfDeath'] = 2023;
+        }
+    }
+    console.log(people);
+    sorted = people.sort((a,b) => (a.yearOfDeath-a.yearOfBirth) < (b.yearOfDeath-b.yearOfBirth) ? 1 : -1);
+    console.log(sorted)
+    return sorted.shift();
 };
 
 // Do not edit below this line
