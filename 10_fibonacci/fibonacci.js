5eba7bc5618a14ea3e7973a5b72e8a79931846b1
tiredDm <ronpri1@gmail.com>
2023-09-12 09:35:05 -0400

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if(n < 0) {
+        return "OOPS";
+    } else if (n == 0){
+        return 0;
+    
+    } else if( n == 1 || n == 2) {
+        return 1; 
+    } else {
+        return (fibonacci(n-1) + fibonacci(n-2));
+    }
 };
 
 // Do not edit below this line
