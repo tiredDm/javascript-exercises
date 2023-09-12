6a4cc1985ecb0ccab01937fb494f045a78946120
tiredDm <ronpri1@gmail.com>
2023-09-12 09:06:54 -0400

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+//prepare input, remove space and punctation...
+input = input.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+input = input.split(' ').join('');
+// splice, split reverse join...
+firstHalf = input.substring(0,Math.floor(input.length/2));
+lastHalf = input.substring(Math.ceil(input.length/2, input.length-1)).split('').reverse().join('');
+console.log(firstHalf + ',' + lastHalf);
+return (firstHalf.toUpperCase() == lastHalf.toUpperCase());
 };
 
 // Do not edit below this line
